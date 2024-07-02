77fc2c18f7af461b4adffa8f315ea4978f4d02c4
Yana Kolomytseva <y.kolomytseva@gmail.com>
2024-07-02 16:33:59 +0200

    path fixed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,18 +7,17 @@ import { FaqPageComponent } from './components/pages/faq-page/faq-page.component
 import { HomeDemoTwoComponent } from './components/pages/home-demo-two/home-demo-two.component';
 import { PricingPageComponent } from './components/pages/pricing-page/pricing-page.component';
 import { ServiceDetailsPageComponent } from './components/pages/service-details-page/service-details-page.component';
-
 import { ServicesPageOneComponent } from './components/pages/services-page-one/services-page-one.component';
 import { TeamPageComponent } from './components/pages/team-page/team-page.component';
 
 const routes: Routes = [
+    { path: '', redirectTo: 'index-2', pathMatch: 'full' }, // Default route
     { path: 'index-2', component: HomeDemoTwoComponent },
     { path: 'about', component: AboutPageComponent },
     { path: 'team', component: TeamPageComponent },
     { path: 'pricing', component: PricingPageComponent },
     { path: 'services', component: ServicesPageOneComponent },
     { path: 'service-details', component: ServiceDetailsPageComponent },
-
     { path: 'clients-feedback', component: ClientsFeedbackPageComponent },
     { path: 'faq', component: FaqPageComponent },
     { path: 'contact', component: ContactPageComponent },
